bfa2e5096a1cf1cedd0d784305da7d0e9ae8d9e1
serionist <osvaysoma@gmail.com>
2018-03-30 10:52:43 +0200

    Finalized for DHIS2 deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  backendUrl: "http://dhis2.starschema.com:85/api/2.8",
+  viewDisplayBaseUrl: "http://demo.virtdb.com:8012/#/views",
+  userName: "holicst",
+  password: "virtdb4ever",
+  siteContentUrl:""
 };
--- tableau-dhis2-backend/tableau-dhis2-backend/Startup.cs
+++ tableau-dhis2-backend/tableau-dhis2-backend/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System.IO;
+using System.Reflection;
 
 namespace tableau_dhis2_backend
 {
@@ -30,7 +32,21 @@ namespace tableau_dhis2_backend
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseCors(e => e.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
-            var uri = new Uri(Configuration["TableauServerBaseUrl"]);
+            Uri uri;
+            var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "ProxyUrl.txt");
+            var log = app.ApplicationServices.GetRequiredService<ILogger<Startup>>();
+            log.LogInformation($"Reading Proxy forward address from '{path}'");
+           
+            try
+            {
+              uri = new Uri(File.ReadAllText(path).Trim());
+              log.LogInformation($"Forwarding to '{uri.AbsoluteUri}'");
+            }
+            catch(Exception e)
+            {
+              log.LogError($"Failed to read ProxyUrl from {path}", e);
+              throw new Exception($"Failed to read ProxyUrl from {path}");
+            }
             app.RunProxy(new ProxyOptions() { Scheme = uri.Scheme, Host = uri.Host, Port = uri.Port.ToString() });
             
         }
